4b33fbe9640022ae699c3446e4af56b96b60884c
Andre Myers <andre.myers99@gmail.com>
2020-04-24 19:47:48 -0400

    mid size screen repsonsiveness

--- app/src/pages/About.js
+++ app/src/pages/About.js
@@ -6,7 +6,7 @@ class About extends Component {
     render() {
         return (
             <div id={"About"}>
-                <div className={'container'}>
+                <div className={'default-container'}>
                     <h2 className={'title'}>My name is Annie Roberts</h2>
                     <p className={'text'}>
                         I am a 23 year old trans woman from Michigan. I create content to give representation to the LGBTQIA+ community.
@@ -14,6 +14,17 @@ class About extends Component {
                         often produced by people with malicious intent. I want to give a real portrayal of a transgender person, by being a charming, skilled, & down to earth human being. One of my ultimate goals is to eventually raise $1 million for charity.
                     </p>
                     <img alt={'annie'} className={'image'} src={"EUs9RyFXsAI9Q2k.jpg"}/>
+                </div>
+                <div className={'mid-size-container'}>
+                    <h2 className={'title'}>My name is Annie Roberts</h2>
+                    <div className={'text-and-image'}>
+                        <p className={'text'}>
+                            I am a 23 year old trans woman from Michigan. I create content to give representation to the LGBTQIA+ community.
+                            A lot of current LGBT media representation contains harmful conspiracies, caricatures,
+                            often produced by people with malicious intent. I want to give a real portrayal of a transgender person, by being a charming, skilled, & down to earth human being. One of my ultimate goals is to eventually raise $1 million for charity.
+                        </p>
+                        <img alt={'annie'} className={'image'} src={"EUs9RyFXsAI9Q2k.jpg"}/>
+                    </div>
                 </div>
             </div>
         )
