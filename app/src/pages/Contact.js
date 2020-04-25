c55c48d045be9bf273091323485a08147070ab73
Andre Myers <andre.myers99@gmail.com>
2020-04-25 04:13:44 -0400

    mid size responisve for contact

--- app/src/pages/Contact.js
+++ app/src/pages/Contact.js
@@ -6,7 +6,7 @@ class About extends Component {
     render() {
         return (
             <div id={"Contact"}>
-                <div className={'container'}>
+                <div className={'default-container'}>
                     <h2 className={'title'}>Get in touch</h2>
                     <p className={'text'}>
                         Let’s talk 👋 Don’t hesitate to reach out! I’m always interested in new opportunities. Shoot me
@@ -18,6 +18,21 @@ class About extends Component {
                         </span>
                     </p>
                     <img alt={'annie'} className={'image'} src={"IMG_20200118_151030_865.jpg"}/>
+                </div>
+                <div className={'mid-size-container'}>
+                    <h2 className={'title'}>Get in touch</h2>
+                    <div className={'text-and-image'}>
+                        <p className={'text'}>
+                            Let’s talk 👋 Don’t hesitate to reach out! I’m always interested in new opportunities. Shoot me
+                            an email at:
+                            <span>
+                            <a className={'link'} href={"mailto:annedromedia@gmail.com"}>
+                             <br/> annedromedia<br/> @gmail.com
+                            </a>
+                        </span>
+                        </p>
+                        <img alt={'annie'} className={'image'} src={"IMG_20200118_151030_865.jpg"}/>
+                    </div>
                 </div>
             </div>
         )
